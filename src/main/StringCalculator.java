d88d229cb5691397f13d9a8e8a6e3a97ffead360
Yogesh_Yerra <yogesh_yerra@epam.com>
2024-02-17 12:27:22 +0530

    2nd commit

--- src/main/StringCalculator.java
+++ src/main/StringCalculator.java
@@ -5,11 +5,16 @@ public class StringCalculator {
         if (numbers.isEmpty())
             return 0;
 
-        int n=numbers.length();
-        int sum=0;
+        int n = numbers.length();
+        int sum = 0;
 
+        String[] split = numbers.split(",");
 
+        for (String s : split) {
+            int i = Integer.parseInt(s);
+            sum += i;
+        }
 
-        return -1;
+        return sum;
     }
 }
