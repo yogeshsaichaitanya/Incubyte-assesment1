458c73a00e23170015286fac77b5e0c853f91e82
Yogesh_Yerra <yogesh_yerra@epam.com>
2024-02-17 13:19:22 +0530

    added null check

--- src/main/StringCalculator.java
+++ src/main/StringCalculator.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 public class StringCalculator {
     public int add(String numbers) {
-        if (numbers.isEmpty())
+        if (numbers == null || numbers.isEmpty())
             return 0;
 
         int n = numbers.length();
